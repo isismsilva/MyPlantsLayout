d97b6435caf23506f5b69ac6173ef389b3f396eb
Isis Silva <isisms2602@gmail.com>
2022-12-10 19:00:41 -0300

    Initial Commit

---
+++ MyPlantsLayout/MyPlantsLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPlantsLayoutApp.swift
+//  MyPlantsLayout
+//
+//  Created by Isis Silva on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MyPlantsLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
